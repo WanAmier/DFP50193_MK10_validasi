c1ecb03a415719e13a2500a7d1ecb4070b4d1177
Arief Hilman <ariefhilman08@gmail.com>
2026-03-20 23:00:39 +0800

    Update IF

--- proses.php
+++ proses.php
@@ -10,17 +10,17 @@ if (isset($_POST['submit'])) {
     $alasan = $_POST['alasan'];
     $perakuan = $_POST['perakuan'] ?? "";
 
-    if (empty($nama) || empty($umur) || empty($tarikh) || empty($program) || empty($jantina) || empty($alasan) || empty($perakuan)) {
-        $_SESSION['error'] = "Sila pastikan semua input diisi.";
+    if (empty($nama) || empty($umur) || empty($tarikh) || empty($program) || empty($jantina) || empty($alasan) || empty($perakuan) || strlen($alasan) < 25) {
+        
+        if (empty($nama) || empty($umur) || empty($tarikh) || empty($program) || empty($jantina) || empty($alasan) || empty($perakuan)) {
+            $_SESSION['error'] = "Sila pastikan semua input diisi.";
+        } else {
+            $_SESSION['error'] = "Alasan mestilah sekurang-kurangnya 25 aksara.";
+        }
+        
         header("Location: index.php");
         exit();
-    } 
-    elseif (strlen($alasan) < 25) {
-        $_SESSION['error'] = "Alasan mestilah sekurang-kurangnya 25 aksara.";
-        header("Location: index.php");
-        exit();
-    } 
-    else {
+    } else {
         $_SESSION['data'] = [
             'nama' => $nama,
             'alasan' => $alasan
